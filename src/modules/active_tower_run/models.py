f1e47390a2b5b2cc4e2bd89a8a7d7df309378eec
sarahstxs <rebeccasarah.book2@gmail.com>
2026-08-22 11:54:49 -0300

    feat(model): Add ActiveTowerRun class

---
+++ src/modules/active_tower_run/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer, Boolean, Float, ForeignKey, PrimaryKeyConstraint, DateTime, Nullable
+from core.database import Base
+
+class ActiveTowerRun(Base):
+    __tablename__ = "active_tower_runs"
+
+    id = Column("id", Integer, primary_key=True, index=True, autoIncrement=True, nullable=False, unique=True)
+    type_tower = Column("type_tower", Integer, nullable=False, unique=False)
+    current_floor = Column("current_floor", Integer, nullable=False, unique=False)
+    active_buff = Column("active_buff", Integer, nullable=False, unique=False)
+    place = Column("place", Integer, ForeignKey("places.id"), nullable=False, unique=False)
+
+    def __init__(self, type_tower, current_floor, active_buff, place):
+        self.type_tower = type_tower
+        self.current_floor = current_floor
+        self.active_buff = active_buff
+        self.place = place
