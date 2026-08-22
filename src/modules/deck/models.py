fa2ea3149bc4b4245c7cc6bcc84073f8271adbc0
sarahstxs <rebeccasarah.book2@gmail.com>
2026-08-22 11:22:36 -0300

    feat(model): Add Deck  class

---
+++ src/modules/deck/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Integer, Boolean, Float, ForeignKey, PrimaryKeyConstraint, DateTime, Nullable
+from core.database import Base
+from datetime import datetime
+
+class Deck(Base):
+    id = Column("id", Integer, primary_key=True, index=True, autoIncrement=True, nullable=False, unique=True)
+    create_date = Column("create_date", DateTime, nullable=False, unique=False)
+    user = Column("user", Integer, ForeignKey("users.id"), nullable=False, unique=False)
+
+    def __init__(self, user, create_date: datetime = None):
+        self.user = user
+        self.create_date = create_date or datetime.now()
