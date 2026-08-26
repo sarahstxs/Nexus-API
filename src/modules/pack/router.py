65208c298f467ce666ef68a2dba42e354a3e9d05
sarahstxs <rebeccasarah.book2@gmail.com>
2026-08-26 11:18:06 -0300

    Adjusting route names

--- src/modules/pack/router.py
+++ src/modules/pack/router.py
@@ -5,30 +5,31 @@ from src.modules.pack.schemas import PackSchema
 from sqlalchemy.orm import Session
 from src.common.dependencies import get_session
 
-pack_routes = APIRouter(prefix="/pack", tags=["pack"])
+pack_routes = APIRouter(prefix="/packs", tags=["pack"])
 
 @pack_routes.get("/")
 async def hero():
     return {"mensagem": "Você acessou a rota de pacotes!"}
 
-@pack_routes.get("/list-all-packs")
+@pack_routes.get("/")
 async def listPacks(session: Session = Depends(get_session)):
     result = await listAllPacks(session)
     return result
 
-@pack_routes.post("/create-pack")
+@pack_routes.get("/{id}")
+async def ListPack(id_pack: int, session: Session = Depends(get_session)):
+    result = await listPack(id_pack=id_pack, session=session)
+    return result
+
+@pack_routes.post("/")
 async def CreatePack(
     pack: PackSchema, 
     session: Session = Depends(get_session)):
     result = await createPack(pack_schema=pack, session=session)
     return result
 
-@pack_routes.delete("/delete-pack/{id_pack}")
+@pack_routes.patch("/{id}")
 async def DeletePack(id_pack: int, session: Session = Depends(get_session)):
     result = await deletePack(id_pack=id_pack, session=session)
     return result
 
-@pack_routes.get("/list=pack")
-async def ListPack(id_pack: int, session: Session = Depends(get_session)):
-    result = await listPack(id_pack=id_pack, session=session)
-    return result
