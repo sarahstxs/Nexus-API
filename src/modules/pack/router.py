7c37e9ae218a57e3adf5f9c8fdf435e3e5d7e6e5
sarahstxs <rebeccasarah.book2@gmail.com>
2026-09-01 11:15:14 -0300

    Adjusting pack routes and classes

--- src/modules/pack/router.py
+++ src/modules/pack/router.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
-import httpx
+from fastapi import APIRouter, Depends
 from src.modules.pack.service import listAllPacks, createPack, desativatePack, listPack, listHeroPack, listAllHeroPack, activatePack, listActivePack, listActivePackByName
 from src.modules.user.service import verificate_token
 from src.modules.pack.schemas import PackSchema
@@ -7,29 +6,43 @@ from sqlalchemy.orm import Session
 from src.common.dependencies import get_session
 
 pack_routes = APIRouter(prefix="/packs", tags=["packs"])
-#
+
 @pack_routes.get("/")
 async def pack():
     return {"mensagem": "Você acessou a rota de pacotes!"}
 
 @pack_routes.get("/list")
-async def ListAllPacks(session: Session = Depends(get_session)):
+async def ListAllPacks(
+    session: Session = Depends(get_session)
+    ):
     result = await listAllPacks(session)
     return result
 
 @pack_routes.get("/list/{id}")
-async def ListPack(id_pack: int, session: Session = Depends(get_session)):
-    result = await listPack(id_pack=id_pack, session=session)
+async def ListPack(
+    id_pack: int, 
+    session: Session = Depends(get_session)
+    ):
+    result = await listPack(
+        id_pack=id_pack, 
+        session=session)
     return result
 
 @pack_routes.get("/list-active")
-async def ListActivePack(session: Session = Depends(get_session)):
-    result = await listActivePack(session=session)
+async def ListActivePack(
+    session: Session = Depends(get_session)
+    ):
+    result = await listActivePack(
+        session=session)
     return result
 
 @pack_routes.get("/list-active-name/{name_pack}")
-async def ListActivePackByName(name_pack: str, session: Session = Depends(get_session)):
-    result = await listActivePackByName(session=session, name_pack=name_pack)
+async def ListActivePackByName(
+    name_pack: str, 
+    session: Session = Depends(get_session)):
+    result = await listActivePackByName(
+        session=session, 
+        name_pack=name_pack)
     return result
 
 @pack_routes.post("/")
@@ -38,32 +51,52 @@ async def CreatePack(
     session: Session = Depends(get_session),
     user = Depends(verificate_token) 
     ):
-    result = await createPack(pack_schema=pack, session=session, user=user)
+    result = await createPack(
+        pack_schema=pack, 
+        session=session, 
+        user=user)
     return result
 
 @pack_routes.patch("/desativate/{id}")
-async def DesativatePack(id_pack: int,
-                      session: Session = Depends(get_session),
-                      user = Depends(verificate_token) ):
-    result = await desativatePack(id_pack=id_pack, session=session, user=user)
+async def DesativatePack(
+    id_pack: int,
+    session: Session = Depends(get_session),
+    user = Depends(verificate_token)
+    ):
+    result = await desativatePack(
+        id_pack=id_pack, 
+        session=session, 
+        user=user)
     return result
 
 @pack_routes.patch("/activate/{id}")
-async def ActivatePack(id_pack: int,
-                      session: Session = Depends(get_session),
-                      user = Depends(verificate_token) ):
-    result = await activatePack(id_pack=id_pack, session=session, user=user)
+async def ActivatePack(
+    id_pack: int,
+    session: Session = Depends(get_session),
+    user = Depends(verificate_token)
+    ):
+    result = await activatePack(
+        id_pack=id_pack, 
+        session=session, 
+        user=user)
     return result
 
 ##############* Hero_pack *#######################
 
 @pack_routes.get("/list-hero-pack/{id}")
-async def ListHeroPack(id_hero_pack: int,
-                        session: Session = Depends(get_session)):
-    result = await listHeroPack(session=session, id_hero_pack=id_hero_pack)
+async def ListHeroPack(
+    id_hero_pack: int,
+    session: Session = Depends(get_session)
+    ):
+    result = await listHeroPack(
+        session=session, 
+        id_hero_pack=id_hero_pack)
     return result
 
 @pack_routes.get("list-All-hero-pack")
-async def ListAllHero(session: Session = Depends(get_session)):
-    result = await listAllHeroPack(session=session)
+async def ListAllHero(
+    session: Session = Depends(get_session)
+    ):
+    result = await listAllHeroPack(
+        session=session)
     return result
--- src/modules/pack/service.py
+++ src/modules/pack/service.py
@@ -1,8 +1,4 @@
-import os
-import httpx
-from fastapi import APIRouter, Depends, HTTPException
-from src.modules.pack.schemas import PackSchema
-from src.common.dependencies import get_session
+from fastapi import  HTTPException
 from sqlalchemy.orm import Session
 from src.modules.pack.models import Pack
 from src.modules.hero_pack.models import HeroPack
@@ -10,13 +6,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-async def listAllPacks(session):
+async def listAllPacks(
+        session):
     list = session.query(Pack).all()
     return {"Packs": list}
 
-async def createPack(session, pack_schema, user):
+async def createPack(
+        session, 
+        pack_schema, 
+        user):
     if not user.admin:
-        raise HTTPException(status_code=403,detail="Você não tem permissão para criar pacotes!")
+        raise HTTPException(
+            status_code=403,
+            detail="Você não tem permissão para criar pacotes!")
     
     name = pack_schema.name,
     deck = pack_schema.deck
@@ -36,43 +38,64 @@ async def createPack(session, pack_schema, user):
     session.refresh(final_pack)
     return {"mensagem": f"Pacote cadastrado com sucesso "}
 
-async def desativatePack(session, id_pack, user):
+async def desativatePack(
+        session, 
+        id_pack, 
+        user):
+    
     if not user.admin:
-        raise HTTPException(status_code=403,detail="Você não tem permissão para desativar esse pacote")
+        raise HTTPException(
+            status_code=403,
+            detail="Você não tem permissão para desativar esse pacote")
     
     pack = session.query(Pack).filter(Pack.id == id_pack).first()
 
     if not pack:
-        raise HTTPException(status_code=404, detail="Pacote não encontrado!")
+        raise HTTPException(
+            status_code=404, 
+            detail="Pacote não encontrado!")
     
     pack.active = False
     session.commit()
     return {"mensagem": "Pacote desativado com sucesso!",
             "Pacote": pack}
 
-async def activatePack(session, id_pack, user):
+async def activatePack(
+        session, 
+        id_pack, 
+        user):
+    
     if not user.admin:
-        raise HTTPException(status_code=403,detail="Você não tem permissão para ativar esse pacote")
+        raise HTTPException(
+            status_code=403,
+            detail="Você não tem permissão para ativar esse pacote")
     
     pack = session.query(Pack).filter(Pack.id == id_pack).first()
 
     if not pack:
-        raise HTTPException(status_code=404, detail="Pacote não encontrado!")
+        raise HTTPException(
+            status_code=404, 
+            detail="Pacote não encontrado!")
     
     pack.active = True
     session.commit()
     return {"mensagem": "Pacote ativado com sucesso!",
             "Pacote": pack}
 
-async def listPack(session, id_pack):
+async def listPack(
+        session, 
+        id_pack):
     pack = session.query(Pack).filter(Pack.id == id_pack).first()
     return pack
 
-async def listActivePack(session):
+async def listActivePack(
+        session):
     pack = session.query(Pack).filter(Pack.active == True).all()
     return {"Packs": pack}
 
-async def listActivePackByName(session, name_pack):
+async def listActivePackByName(
+        session, 
+        name_pack):
     packs = session.query(Pack).filter(
         Pack.name.icontains(name_pack),
         Pack.active == True
@@ -80,10 +103,13 @@ async def listActivePackByName(session, name_pack):
     
     return packs
 
-async def listHeroPack(session, id_hero_pack):
+async def listHeroPack(
+        session, 
+        id_hero_pack):
     hero_pack = session.query(HeroPack).filter(HeroPack.id == id_hero_pack).first()
     return hero_pack
 
-async def listAllHeroPack(session):
+async def listAllHeroPack(
+        session):
     list_hero_pack = session.query(HeroPack).all()
     return {"Hero_packs": list_hero_pack}
